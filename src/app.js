8e5bfa40e4534239f59f01f28f81e7c4c6c62c2e
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-04 11:31:39 -0300

    feat: create Data.js and move users and tweets

--- src/app.js
+++ src/app.js
@@ -1,78 +1,14 @@
 import express from "express"
 import cors from "cors"
-
+import {users, tweets} from "./data.js"
+  
 const app = express()
 const port = 5000
 
 app.use(cors())
 app.use(express.json())
 
-const users = [
-  {
-    username: 'bobesponja',
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-  },
-    {
-    username: 'lubawski',
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-  },
-    {
-    username: 'nathan',
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-  },
-]
-const tweets = [
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  },
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  },
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  },
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  },
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  }, {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  }, {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  }, {
-    username: "lubawski",
-    tweet: "eu amo o cccccccccc"
-  }, {
-    username: "nathan",
-    tweet: "xxxxxxxxx"
-  }, {
-    username: "08300442995",
-    tweet: "eu amo o gggghub"
-  }, {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  },
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  },
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  },
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  },
-]
+
 
 
 app.post('/sign-up', (req, res) => {
