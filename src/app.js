83fedfa327ce6f0b7836aabf1e23a885db3e582b
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-04 08:43:50 -0300

    feat: create users and tweets

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import express from "express"
 
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.send('Hello')
 })
+
+const users = [
+  {
+    username: 'bobesponja',
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+  },
+]
+
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub"
+  }
+]
+
 
 app.listen(port, () => {
   console.log(`Rodando na porta ${port}`)
