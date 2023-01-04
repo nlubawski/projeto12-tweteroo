919545840f2dcfcb43ed8c37833e3c50d9dcff5d
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-04 09:43:04 -0300

    fix: adicioned json for use body in post

--- src/app.js
+++ src/app.js
@@ -5,16 +5,24 @@ const app = express()
 const port = 5000
 
 app.use(cors())
+app.use(express.json())
 
 app.post('/sign-up',(req,res) => {
+  const {avatar, username} = req.body
   users.push({username, avatar})
-  res.send(200)
+  res.sendStatus(200)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello')
+app.get('/tweets', (req, res) => {
+  res.send(test)
 })
 
+const test = [{
+    username: 'bobesponja',
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub"
+
+  }]
 const users = [
   {
     username: 'bobesponja',
